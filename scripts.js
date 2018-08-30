e47a616f8c13068dc63ffa307da5135a28292708
JennSung <jennifer.sung@colorado.edu>
2018-08-29 19:42:46 -0600

    configure match capability to cards

--- scripts.js
+++ scripts.js
@@ -1,7 +1,34 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
 }
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
+}
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
